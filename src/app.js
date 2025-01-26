5f5154fced0dd84e3760312e6abd3b24ad071698
nitin bahuguna <nitinbahuguna1251@gmail.com>
2025-01-26 17:59:50 +0530

    Expolring http methods

--- src/app.js
+++ src/app.js
@@ -2,10 +2,22 @@ const express = require('express');
 
 const app = express();
 
+// This will only handle the GET requests
+app.get("/", (req, res) => {
+    res.send("Hello From The Server Through GET...")
+})
+
+// This will only handle the POST requests
+app.post("/", (req, res) => {
+    res.send("Hello From The Server Through POST...")
+})
+
+// This will handle all requests (GET, POST, PUT, DELETE)
 app.use("/", (req, res) => {
     res.send("Hello From The Server...")
 })
 
+// This will handle all requests (GET, POST, PUT, DELETE) for the /test path
 app.use("/test", (req,res)=> {
     res.send("This Is The Test Response From The Server...")
 })
