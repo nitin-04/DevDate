b12536bbf719de2ae9739c99df226c4c44728a0b
nitin bahuguna <nitinbahuguna1251@gmail.com>
2025-01-28 10:15:21 +0530

    Add multiple response handlers  / middleware

--- src/app.js
+++ src/app.js
@@ -22,6 +22,34 @@ app.use("/test", (req,res)=> {
     res.send("This Is The Test Response From The Server...")
 })
 
+// only one response can be sent in a single request even if the next() is called
+app.use("/test1", (req,res, next) =>{
+    // res.send("This Is The Test1 Response From The Server...")
+    next(); //next() is called to pass the control to the next middleware
+},
+(req,res, next) =>{
+    // res.send("This Is The Test2 Response From The Server...")
+    next()
+},
+
+(req,res) =>{
+    res.send("This Is The Test3 Response From The Server...") //actual request handler response
+},
+(req,res) =>{
+    res.send("This Is The Test4 Response From The Server...")
+}
+);
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(4444, () => {
     console.log('Server is running on port 4444');
