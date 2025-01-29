524709142e3157bec3428672270a198822a5dbef
nitin bahuguna <nitinbahuguna1251@gmail.com>
2025-01-29 10:30:10 +0530

    auth middleware

--- src/app.js
+++ src/app.js
@@ -2,20 +2,38 @@ const express = require('express');
 
 const app = express();
 
+
+// const {userAuth, adminAuth} = require("./auth");
+
+// // Middleware to check if user is authenticated or not
+// app.use("/user", userAuth);
+
+// // Middleware to check if user is admin or not
+// app.get("/admin", adminAuth) = (req, res) => {
+//     res.send("Welcome To The Admin Page...");
+// };
+
+// app.get("/admin/deleteUser", (req, res) => {
+//     res.send("User Deleted Successfully...");
+// })
+
+
+
+
 // This will only handle the GET requests
-app.get("/", (req, res) => {
-    res.send("Hello From The Server Through GET...")
-})
+// app.get("/", (req, res) => {
+//     res.send("Hello From The Server Through GET...")
+// })
 
 // This will only handle the POST requests
-app.post("/", (req, res) => {
-    res.send("Hello From The Server Through POST...")
-})
+// app.post("/", (req, res) => {
+//     res.send("Hello From The Server Through POST...")
+// })
 
 // This will handle all requests (GET, POST, PUT, DELETE)
-app.use("/", (req, res) => {
-    res.send("Hello From The Server...")
-})
+// app.use("/", (req, res) => {
+//     res.send("Hello From The Server...")
+// })
 
 // This will handle all requests (GET, POST, PUT, DELETE) for the /test path
 app.use("/test", (req,res)=> {
@@ -39,17 +57,6 @@ app.use("/test1", (req,res, next) =>{
     res.send("This Is The Test4 Response From The Server...")
 }
 );
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(4444, () => {
     console.log('Server is running on port 4444');
