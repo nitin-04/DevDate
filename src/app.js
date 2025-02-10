4a0d2b3856ec80e5f5f2e36cf18068d5a8c42192
nitin bahuguna <nitinbahuguna1251@gmail.com>
2025-02-10 20:27:29 +0530

    Add user management endpoints and validation for user model

--- src/app.js
+++ src/app.js
@@ -12,21 +12,97 @@ app.post("/signup", async (req, res) => {
     console.log(req.body);
 
     const user = new User(req.body);
-    try{
+    try {
         await user.save();
         res.send("User Added Successfully..!")
     }
-    catch(err){
+    catch (err) {
         res.status(400).send("Error saving user: " + err.message);
     };
 });
 
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+    try {
+        const user = await User.findOne({ emailId: userEmail });
+        if (!user) {
+            res.status(404).send("User emalID not found")
+        }
+        else {
+            res.send(user);
+        }
+    }
+    catch {
+        res.status(404).send("User emalID not found")
+    }
+})
 
 
+//Feed API - GET/ feed -- get all the users from Database
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+    }
+    catch (err) {
+        res.status(404).send("Error getting feed");
+    }
+})
+
+//Delete User from the Database
+app.delete("/user", async (req, res) => {
+    const userId = req.body.userId;
+    try {
+        const user = await User.findByIdAndDelete(userId);
+        if (!user) {
+            res.status(404).send("User ID not found")
+        }
+        else {
+            res.send("User deleted Successfully..!")
+        }
+    }
+
+    catch {
+        res.status(404).send("Error deleting user");
+    }
+})
+
+//Update user
+app.patch("/user/:userId", async (req, res) => {
+    const userId = req.params?.userId;
+    const data = req.body;
+
+    try {
+        const ALLOWED_UPDATES = [
+            "about",
+            "skills",
+            "gender",
+            "age",
+
+        ];
+        const isUpdateAllowed = Object.keys(data).every((k) =>
+            ALLOWED_UPDATES.includes(k));
+
+        if (!isUpdateAllowed) {
+            throw new Error("Update Not Allowed");
+        }
+
+        if(data?.skills.length > 12){
+            throw new Error("Skills can't exceed 12");
+        }
+
+
+        await User.findByIdAndUpdate({ _id: userId }, data, { runValidators: true })
+        res.send("User updated successfully..")
+    }
+    catch (err) {
+        res.status(404).send("UPDATE FAILED:" + err.message)
+    }
+})
 
 mongooseDB()
     .then(() => {
-        console.log("connection Established Successfully")
+        console.log("connection Established Successfully...")
         app.listen(4444, () => {
             console.log('Server is running on port 4444');
         });
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,64 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
+        required: true, //Mandatory
     },
     lastName: {
         type: String,
     },
     emailId: {
-        tupe: String,
+        type: String,
+        lowercase: true,
+        trim: true, //remove whitespaces
+        required: true,
+        unique: true, // Unique
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Invalid Email: " + value);
+            }
+        }
     },
     password: {
         type: String,
+        required: true,
+        validate(value) {
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Enter a strong password: " + value);
+            };
+        },
     },
     age: {
         type: String,
+        min: 18,
     },
     gender: {
         type: String,
+        validate(value){
+            if(!["male", "female", "other"].includes(value)){
+                throw new Error("Gender should be either male, female or other");
+            }
+        }
     },
+    photoUrl:{
+        type: String,
+        validate(value) {
+            if(!validator.isURL(value)){
+                throw new Error("Invalid URL" + value);
+            };
+        },
+    },
+    about:{
+        type: String,
+        default: "This is a default description"
+    },
+    skills:{
+        type:[String],
+    },
+},{
+    timestamps:true,
 });
 
 module.exports = mongoose.model("User", userSchema);
