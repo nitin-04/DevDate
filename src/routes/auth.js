b69ac145c82b348ae98ce9b26c5a4fda84874720
nitin bahuguna <nitinbahuguna1251@gmail.com>
2025-02-12 15:58:06 +0530

    Add logout endpoint and profile editing functionality with validation

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -60,5 +60,9 @@ authRouter.post("/login", async (req, res) => {
 })
 
 
+authRouter.post("/logout", async (req, res) => {
+    res.cookie("token", null, { expires: new Date(Date.now()), })
+    res.send("Logout Sucessfully.....");
+});
 
 module.exports = authRouter;
