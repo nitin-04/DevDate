e00a1486351a94e4c9a52cb792f3b3e549096e9d
nitin bahuguna <nitinbahuguna1251@gmail.com>
2025-02-14 20:07:09 +0530

    login credentials

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -45,8 +45,10 @@ authRouter.post("/login", async (req, res) => {
         if (isPasswordValid) {
 
             const token = await user.getJWT();
-            res.cookie("token", token);
-            res.send("Login Successfully...")
+            res.cookie("token", token, {
+                expires: new Date(Date.now() + 8 *3600000),
+            });
+            res.send(user)
         }
         else {
             throw new Error("Invalid Credentials")
