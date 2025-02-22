b30ed1ca9976501f096b9f5de64295fcc893bbcf
nitin bahuguna <nitinbahuguna1251@gmail.com>
2025-02-22 19:11:49 +0530

    Update CORS origin to new deployment URL

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 app.use(
     cors({
         // origin: "http://localhost:5173" ,
-        origin: "https://devdate-navy.vercel.app",
+        origin: "https://devdatee.vercel.app/",
         credentials: true,
 
     }))
